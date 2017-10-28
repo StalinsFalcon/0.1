a6f408fefd5bfc7477692b49cffb5ae7c16e7093
Artyom Rudakov <two_dollars@icloud.com>
2017-10-28 23:54:18 +0300

    Тестовый коммит

---
+++ Xcode/Xcode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Xcode
+//
+//  Created by TwoDollarsEsq on 10/28/17.
+//  Copyright © 2017 TwoDollarsEsq. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
