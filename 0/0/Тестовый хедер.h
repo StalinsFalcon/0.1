f4b09ac5c211c8c3c1ffb0fc8da61232b7edaf6f
Artyom Rudakov <two_dollars@icloud.com>
2017-10-28 23:22:30 +0300

    Новый коммит для проверки

---
+++ 0/0/Тестовый хедер.h
@@ -0,0 +1,13 @@
+//
+//  Тестовый хедер.h
+//  0
+//
+//  Created by TwoDollarsEsq on 10/28/17.
+//  Copyright © 2017 TwoDollarsEsq. All rights reserved.
+//
+
+#ifndef ________________h
+#define ________________h
+
+
+#endif /* ________________h */
